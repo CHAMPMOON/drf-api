b82432aacc226b31929803aa9ecb87d44a156dce
CHAMPMOON <antipov8v@gmail.com>
2022-04-21 22:22:04 +0300

    update Comment table

--- comment/models.py
+++ comment/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Comment(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    path = models.TextField()
+    depth = models.IntegerField()
